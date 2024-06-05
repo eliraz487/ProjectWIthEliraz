50cb7be47aa60ec6ab6dfc0941a90676682d8e34
Eliraz <manraz487@gmail.com>
2024-06-05 18:22:36 +0300

    Merge remote-tracking branch 'origin/master'

--- src/containers/header/links.jsx
+++ src/containers/header/links.jsx
@@ -1,6 +1,6 @@
 export const guestLinks = [
   { text: 'Home', href: '' },
-  { text: 'What is GPT3?', href: '#wgpt3' },
+  { text: 'What is GPT4?', href: '#wgpt3' },
   { text: 'Open AI', href: '#possibility' },
   { text: 'Case Studies', href: '#features' },
   { text: 'Library', href: '#blog' },
@@ -8,7 +8,7 @@ export const guestLinks = [
 
 export const userLinks = [
   { text: 'Home', href: '' },
-  { text: 'What is GPT3?', href: '#wgpt3' },
+  { text: 'What is GPT4?', href: '#wgpt3' },
   { text: 'Open AI', href: '#possibility' },
   { text: 'Case Studies', href: '#features' },
   { text: 'Library', href: '#blog' },
